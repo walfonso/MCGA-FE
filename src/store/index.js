a971a75ac338b0078f4c94845e00ee988e2ea7dc
walfonso <walfonso@hotmail.com>
2023-11-28 20:03:48 -0300

    update store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,37 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import reducer from './reducers';
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+//import reducer from "./reducers";
+import { authReducer } from "./reducers/authReducer";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import usersReducer from "./reducers/usersReducer";
+import productsReducer from "./reducers/productsReducer";
+import clientsReducer from "./reducers/clientsReducer";
+import suppliersReducer from "./reducers/suppliersReducer";
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["authState"],
+};
+
+const rootReducer = combineReducers({
+  authState: authReducer,
+  auth: authReducer,
+  users: usersReducer,
+  products: productsReducer,
+  clients: clientsReducer,
+  suppliers: suppliersReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = createStore(
-  reducer,
+  persistedReducer,
   compose(
     applyMiddleware(thunk),
 
-    typeof window === 'object' &&
-      typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
+    typeof window === "object" &&
+      typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
       ? window.__REDUX_DEVTOOLS_EXTENSION__()
       : (f) => f
   )
