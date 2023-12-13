d5a4d7d4c9d5f4c5f4567eab151c686ca80c6597
walfonso@hotmail.com <walfonso@hotmail.com>
2023-12-13 00:02:13 -0300

    add authentication Sidebar

--- src/shared/Sidebar/index.js
+++ src/shared/Sidebar/index.js
@@ -1,26 +1,36 @@
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 import style from "./Sidebar.module.css";
 
 const Sidebar = () => {
+  const user = useSelector((state) => state.authState);
   return (
     <div className={style.sidebar}>
       <ul>
-        <li>
-          <Link to="/">Dashboard</Link>
-        </li>
+        {user.email && (
+          <>
+            <li>
+              <Link to="/Dashboard">Dashboard</Link>
+            </li>
+          </>
+        )}
         <li>
           <Link to="/products">Productos</Link>
         </li>
-        <li>
-          <Link to="/users">Usuarios</Link>
-        </li>
-        <li>
-          <Link to="/clients">Clientes</Link>
-        </li>
-        <li>
-          <Link to="/suppliers">Proveedores</Link>
-        </li>
+
+        {user.email && (
+          <>
+            <li>
+              <Link to="/users">Usuarios</Link>
+            </li>
+            <li>
+              <Link to="/clients">Clientes</Link>
+            </li>
+            <li>
+              <Link to="/suppliers">Proveedores</Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
